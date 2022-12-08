4a1e603967aefb25888e91dd77cc505b21919c9b
Icedragal <icedragal@gmail.com>
2022-12-08 14:12:03 +0300

    Задание 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
+        Car granta = new Car("Lada", "Granta", 2015, "Россия", "Желтый",110 , 1.7F, "Механическая","универсал","p231jh374",6, false, null, null);
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", 2020, "Германия", "Черный", 3.0F);
+        Car bmw = new Car("BMW", "Z8", 2021, "Германия", "Черный", 3.0F);
+        Car kia = new Car("Kia", "Sportage 4-го поколения", 2018, "Южная Корея", "Крсный", 1.7F);
+        Car hyundai = new Car("Hyundai", "Avante", 2016, "Южная Корея", "Оранжевый", 1.6F);
+        System.out.println(granta);
+        System.out.println(audi);
+        System.out.println(bmw);
+        System.out.println(kia);
+        System.out.println(hyundai);
     }
 }
---
+++ src/Transport.java
@@ -0,0 +1,70 @@
+public class Transport {
+    private final String brand;
+    private final String model;
+    private final int year;
+    private final String country;
+    private String color;
+    private double maxSpeed;
+
+    public Transport(String brand, String model, int year, String country) {
+        this(brand, model, year, country, "Белый", 120);
+    }
+
+    public Transport(String brand, String model, int year, String country, String color, double maxSpeed) {
+        if (brand != null && !brand.isEmpty() && !brand.isBlank()) {
+            this.brand = brand;
+        } else this.brand = "default";
+
+        if (model != null && !model.isEmpty() && !model.isBlank()) {
+            this.model = model;
+        } else this.model = "default";
+
+        if (year > 0) {
+            this.year = year;
+        } else this.year = 2000;
+
+        if (country != null && !country.isEmpty() && !country.isBlank()) {
+            this.country = country;
+        } else this.country = "default";
+
+        setColor(color);
+
+        setMaxSpeed(maxSpeed);
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setColor(String color) {
+        if (color != null && !color.isEmpty() && !color.isBlank()) {
+            this.color = color;
+        } else this.color = "Белый";
+    }
+
+    public void setMaxSpeed(double maxSpeed) {
+        if (maxSpeed > 0) {
+            this.maxSpeed = maxSpeed;
+        } else this.maxSpeed = 120;
+    }
+}
